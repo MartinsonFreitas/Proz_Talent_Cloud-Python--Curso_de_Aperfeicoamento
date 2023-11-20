58b397903eac5f85134795ef3b17c700f3a3023c
Martinson Freitas <martinson.freitas@gmail.com>
2023-11-20 11:30:48 -0300

    Add files via upload

---
+++ arrays3.py
@@ -0,0 +1,21 @@
+"""
+Uma nova loja de cosméticos abriu no seu bairro e pediram para você elaborar um sistema que 
+imprime na tela na frente da loja os novos produtos que chegaram. O sistema da loja já tem um array 
+com os produtos, você precisa apenas imprimir eles no terminal, um por um.
+
+Como desafio opcional, tente imprimir cada produto com a frase 
+"Temos [produto] à venda!" (ex. "Temos máscaras faciais à venda!"). 
+
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 
+'sabonetes', 'delineadores'] 
+
+Trabalhe esse código no Google Colab, e compartilhe o link do projeto no campo ao lado para que outros 
+desenvolvedores possam analisá-lo.    
+"""
+import time
+
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+for produto in lista_produtos:
+    print(f'Temos {produto} à venda!')
+    time.sleep(1)
